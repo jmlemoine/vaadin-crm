89bbf5331b68a56c9f6f153f5f6338d4aef0ff5d
jmlemoine <jeanmelvinlp27@gmail.com>
2020-08-13 19:35:38 -0400

    Login

--- src/main/java/com/vaadin/tutorial/crm/ui/MainLayout.java
+++ src/main/java/com/vaadin/tutorial/crm/ui/MainLayout.java
@@ -3,6 +3,7 @@ package com.vaadin.tutorial.crm.ui;
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.applayout.DrawerToggle;
 import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.html.Anchor;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
@@ -39,9 +40,12 @@ public class MainLayout extends AppLayout {
         H1 logo = new H1("Vaadin CRM");
         logo.addClassName("logo");
 
-        HorizontalLayout header = new HorizontalLayout(new DrawerToggle(), logo);
+        Anchor logout = new Anchor("/logout", "Log Out");
+
+        HorizontalLayout header = new HorizontalLayout(new DrawerToggle(), logo, logout);
         header.addClassName("header");
         header.setWidth("100%");
+        header.expand(logo);
         header.setDefaultVerticalComponentAlignment(FlexComponent.Alignment.CENTER);
 
         addToNavbar(header);
